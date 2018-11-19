04a0441e6466372acb615d4cad9d7bbc72fc61fc
Jessica Pena <jessica@scripted.org>
2018-11-19 18:58:48 +0000

    last changes

--- js/script.js
+++ js/script.js
@@ -2,12 +2,6 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
-$("#shoot").click(function(){
-     var userChoice = $("#input").val();
-     var randomNumber=Math.random();
-     $("#userChoice").html();
-});
-
 var userChoice = "";
 
 var computerChoice = "";
@@ -16,37 +10,46 @@ var winner = "";
 
 var randomNumber = 0;
 
-console.log(randomNumber);
+$("#shoot").click(function(){
+     var userChoice = $("#input").val();
+      $("#userChoice").html(userChoice);
+     var randomNumber=Math.random();
+     
+     console.log(randomNumber);
     if(randomNumber > .80) {
         $("#computerChoice").html("rock"); 
     }else if(randomNumber<.80 && randomNumber>.40){
         $("#computerChoice").html("scissors");
-    }else if (fortune<.40 && fortune>0){
+    }else if (randomNumber<.40 && randomNumber>0){
         $("#computerChoice").html("paper");
     }else {
         $("#computerChoice").html("paper");
-
-console.log(result);
+        
+    }
+    
+    console.log(winner);
     if(computerChoice === "rock" && userChoice === "paper"){
-        $("#result").html("You win!"); 
+        $("#winner").html("You win!"); 
     }else if(computerChoice === "rock" && userChoice === "scissors"){
-        $("#result").html("Computer Wins!");
+        $("#winner").html("Computer Wins!");
     }else if (computerChoice === "rock" && userChoice === "rock"){
-        $("#result").html("No one wins!");
+        $("#winner").html("No one wins!");
     }else if (computerChoice === "paper" && userChoice === "paper"){
-        $("#result").html("No one wins!");
+        $("#winner").html("No one wins!");
     }else if (computerChoice === "paper" && userChoice === "rock"){
-        $("#result").html("Computer wins!");
+        $("#winner").html("Computer wins!");
     }else if (computerChoice === "paper" && userChoice === "scissors"){
-        $("#result").html("You win!");
+        $("#winner").html("You win!");
     }else if (computerChoice === "scissors" && userChoice === "paper"){
-        $("#result").html("Computer wins!");
+        $("#winner").html("Computer wins!");
     }else if (computerChoice === "scissors" && userChoice === "rock"){
-        $("#result").html("You win!");
+        $("#winner").html("You win!");
     }else if (computerChoice === "scissors" && userChoice === "scissors"){
-        $("#result").html("No one wins!");
+        $("#winner").html("No one wins!");
     }else {
-        $("#result").html("No one wins!");
+        $("#winner").html("No one wins!");
+    }
+});
 
 
 
